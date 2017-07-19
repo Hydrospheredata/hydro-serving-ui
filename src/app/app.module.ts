1d0cb13a283275939a68729e5b4643f4eb952735
Andrey Nenashev <proudhamyak@gmail.com>
2017-07-19 12:14:05 +0300

    models-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MdlModule } from '@angular-mdl/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { routing } from '@app/app.router';
+import { HydroRouter } from '@app/app.router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 import { ModelsWrapperComponent } from '@components/models-wrapper/models-wrapper.component';
+import { ModelsListComponent } from '@components/models-wrapper/models-list/models-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ModelsWrapperComponent
+    ModelsWrapperComponent,
+    ModelsListComponent
   ],
   imports: [
     BrowserModule,
     MdlModule,
     FlexLayoutModule,
-    routing
+    HydroRouter
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/models-wrapper/models-list/models-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hydro-models-list',
+  templateUrl: './models-list.component.html',
+  styleUrls: ['./models-list.component.scss']
+})
+export class ModelsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
