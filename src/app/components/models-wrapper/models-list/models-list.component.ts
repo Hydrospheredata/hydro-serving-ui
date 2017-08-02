80466ee34152051b72d9ae2561dba7d888cce1f6
Andrey Nenashev <proudhamyak@gmail.com>
2017-08-02 19:08:56 +0300

    update model after build

--- src/app/components/models-wrapper/models-list/models-list.component.ts
+++ src/app/components/models-wrapper/models-list/models-list.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ModelStore } from '@stores/model.store';
+import { BuildModelService } from '@services/build-model.service';
+import { Model } from '@models/model';
 
 @Component({
   selector: 'hydro-models-list',
   templateUrl: './models-list.component.html',
-  styleUrls: ['./models-list.component.scss']
+  styleUrls: ['./models-list.component.scss'],
+  providers: [BuildModelService]
 })
 export class ModelsListComponent implements OnInit {
-  private  models;
+  public models: Model[];
 
-  constructor(private modelStore: ModelStore) { }
+  constructor(
+    private modelStore: ModelStore,
+    private buildModelService: BuildModelService
+  ) { }
 
   ngOnInit() {
     this.modelStore.getAll()
     this.modelStore.items.subscribe((models) => {
       this.models = models
+    })
+  }
+
+  buildModel(modelId: string, version?: string) {
+    this.buildModelService.build(modelId, version).subscribe((modelRuntime) => {
+      let model = this.models.find((item) => item.id === modelRuntime.modelId)
+      model.lastModelRuntime = modelRuntime;
     })
   }
 
---
+++ src/app/services/build-model.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { ModelRuntime } from '@models/model-runtime';
+import { ModelRuntimeBuilder } from '@builders/model-runtime.builder'
+
+@Injectable()
+export class BuildModelService {
+  private baseUrl: string;
+
+  constructor(
+    private http: Http,
+    private modelRuntimeBuilder: ModelRuntimeBuilder
+  ) {
+    this.baseUrl = `${environment.host}:${environment.port}/api/v1/model/build` 
+  }
+
+  public build(modelId: string, version?: string): Observable<ModelRuntime> {
+    let body = { modelId: modelId, version: version }
+    return this.http.post(this.baseUrl, body).map((res: Response) => {
+      return this.extractModelRuntime(res);
+    })
+  }
+
+  private extractModelRuntime(res: Response): ModelRuntime {
+    let props = res.json();
+    return this.modelRuntimeBuilder.build(props);
+  }
+}
