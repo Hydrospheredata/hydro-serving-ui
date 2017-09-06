8c948eba9c6bf5bceb9d8d5e8158a46c9338d772
Tereshin Maxim <maxim_tereshin@selloutsport.com>
2017-09-06 13:08:49 +0300

    Add sidebar to services

--- src/app/modules/services/services.module.ts
+++ src/app/modules/services/services.module.ts
@@ -1,5 +1,8 @@
 // Импорт зависимостей данного модуля.
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { MdlModule } from '@angular-mdl/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ServicesRoutingModule } from "./services.router";
 
 // Импорт страниц данного модуля.
@@ -9,6 +12,9 @@ import { ServicesWrapperComponent, ServicesListComponent } from "./_index";
 
 @NgModule({
     imports: [
+        FlexLayoutModule,
+        CommonModule,
+        MdlModule,
         ServicesRoutingModule // настройки маршрутизации для модуля LandingModule
     ],
     declarations: [
