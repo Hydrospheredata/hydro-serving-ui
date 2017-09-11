e5e4e3098da0d096bfac153ab8292ba8064383fb
Tereshin Maxim <maxim_tereshin@selloutsport.com>
2017-09-11 13:09:28 +0300

    Fix app and services wrapper

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,31 +1,3 @@
-// import {ModuleWithProviders}  from '@angular/core';
-// import {Routes, RouterModule} from '@angular/router';
-// import {SingleModelComponent} from '@components/models-wrapper/models-list/single-model/single-model.component';
-
-// import {ModelsWrapperComponent} from '@components/models-wrapper/models-wrapper.component';
-// import {ModelsListComponent} from '@components/models-wrapper/models-list/models-list.component';
-
-// // Route Configuration
-// export const routes: Routes = [
-//     {
-//       path: '',
-//       redirectTo: '/models/all',
-//       pathMatch: 'full'
-//     }, {
-//     path: 'models',
-//     component: ModelsWrapperComponent,
-//     children: [{
-//       path: ':modelId',
-//       pathMatch: 'prefix',
-//       component: ModelsListComponent
-//     }]
-//   }]
-// ;
-
-// export const HydroRouter: ModuleWithProviders = RouterModule.forRoot(routes);
-
-
-
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -33,6 +5,6 @@ import { RouterModule } from '@angular/router';
     imports: [RouterModule.forRoot([
 
     ])],
-    exports: [RouterModule] // делаем re-export модуля для использования директив при маршрутизации
+    exports: [RouterModule]
 })
 export class HydroRouter { }
