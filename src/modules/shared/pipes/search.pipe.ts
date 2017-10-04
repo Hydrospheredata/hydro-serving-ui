fb4d61b50f2758a9fd887454f664c9156a13e2c4
Tereshin Maxim <maxim_tereshin@selloutsport.com>
2017-10-04 19:04:27 +0300

    Femove logs

--- src/modules/shared/pipes/search.pipe.ts
+++ src/modules/shared/pipes/search.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
     transform(items: any[] = [], field: any, q: string): any {
-        console.log(items, field, q);
         if (!items || !field || !q) return items;
         return items.filter((item) => {
             const result = item[field].toLowerCase().indexOf(q.toLowerCase()) !== -1;
