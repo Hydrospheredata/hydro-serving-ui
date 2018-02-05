2e8b613d7bfe54757dd9b32e89c18d517c92bda1
Maxim Tereshin <tereshin93@gmail.com>
2018-02-05 12:17:00 +0300

    Fix #69

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { MdlDialogOutletService } from '@angular-mdl/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '@shared/models/_index';
 import * as Actions from '@shared/actions/_index';
@@ -14,8 +15,12 @@ export class AppComponent {
     title = 'HydroServingUi';
 
     constructor(
+        private dilalogOuletService: MdlDialogOutletService, 
+        private viewConatinerRef: ViewContainerRef,
         private store: Store<AppState>,
-    ) {}
+    ) {
+        this.dilalogOuletService.setDefaultViewContainerRef(this.viewConatinerRef);
+    }
 
     ngOnInit() {
         this.store.dispatch({ type: Actions.LOAD_MODELS, payload: null });
