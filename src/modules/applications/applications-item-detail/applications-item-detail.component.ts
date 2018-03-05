d50592590681df60831bee2200d692e9bf800bcc
Maxim Tereshin <tereshin93@gmail.com>
2018-03-06 00:18:00 +0300

    Added application plot with alerts

--- src/modules/applications/applications-item-detail/applications-item-detail.component.ts
+++ src/modules/applications/applications-item-detail/applications-item-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectorRef, ElementRef, Renderer2 } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MdlDialogService } from '@angular-mdl/core';
 import { Subscription } from 'rxjs/Subscription';
+import { Chart } from "chart.js";
 
 import { Store } from '@ngrx/store';
 import { 
@@ -21,6 +22,7 @@ import {
     injectableServiceUpdate
 } from '@components/dialogs/_index';
 
+declare var EventSource:any;
 
 
 
@@ -28,9 +30,8 @@ import {
     selector: 'hydro-applications-item-detail',
     templateUrl: './applications-item-detail.component.html',
     styleUrls: ['./applications-item-detail.component.scss'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
 })
-
 export class ApplicationsItemDetailComponent {
     public JSON = JSON;
     public id: string = '';
@@ -61,7 +62,16 @@ export class ApplicationsItemDetailComponent {
         }
     };
 
+    public alerts: any[] = [];
+    public chart: any;
+    public chartData = {
+        labels: [],
+        datasets: []
+    };
 
+    private eventSourcePath = "http://localhost:9999/v1/measure/streaming";
+    private eventSourceAlerts: any;
+    private eventSourceMeasures: any;
     private storeSub: Subscription;
     private activeRouteSub: Subscription;
     private runtimesStoreSub: Subscription;
@@ -71,9 +81,11 @@ export class ApplicationsItemDetailComponent {
         public dialog: MdlDialogService,
         private activatedRoute: ActivatedRoute,
         // private modelServicesService: ModelServicesService,
-        private router: Router
+        private router: Router,
+        private changeDetector: ChangeDetectorRef,
+        private elementRef: ElementRef,
+        private renderer: Renderer2,
     ) {
-
         this.activeRouteSub = this.activatedRoute.params
             .map(params => {
                 this.id = params['id'];
@@ -84,20 +96,73 @@ export class ApplicationsItemDetailComponent {
                 if (this.storeSub) {
                     this.storeSub.unsubscribe();
                 }
+                if (this.eventSourceAlerts) {
+                    this.eventSourceAlerts.close();
+                }
+                if (this.eventSourceMeasures) {
+                    this.eventSourceMeasures.close();   
+                }
                 this.loadInitialData(id);
             });
     }
 
-    chartData = [
-        { data: [330, 600, 260, 700, 260, 700], label: 'Account A' },
-        { data: [120, 455, 100, 340, 260, 700], label: 'Account B' },
-        { data: [45, 67, 800, 500, 260, 700], label: 'Account C' }
-    ];
+    ngAfterContentInit() {
+        this.initChart();
+    }
 
-    chartLabels = ['n-5', 'n-4', 'n-3', 'n-2', 'n-1', 'n'];
+    public node: string;
 
-    public onChartClick(event) {
-        console.log(event);
+    public initChart() {
+        Chart.defaults.global.defaultFontColor = '#04143c';
+        Chart.defaults.global.defaultFontFamily = '"Museo Sans Regular"';
+        
+        let containerRef = this.elementRef.nativeElement.querySelector('.content');
+        let chartRef = this.renderer.createElement('canvas');
+        this.renderer.appendChild(containerRef, chartRef);
+        
+        this.chart = new Chart(chartRef, {
+                type: 'bar',
+                data: this.chartData,
+                options: {
+                    animate: false,
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Application graph',
+                        fontSize: 20,
+                    },
+                    scales: {
+                        yAxes: [{
+                            id: 'left-y-axis',
+                            type: 'linear',
+                            position: 'left',
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Confidence',
+                                fontSize: 16,
+                            }
+                        }, {
+                            id: 'right-y-axis',
+                            type: 'linear',
+                            position: 'right',
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Classes\' average values',
+                                fontSize: 16,
+                            }
+                        }],
+                        xAxes: [
+                            {
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Timeline',
+                                    fontSize: 16,
+                                }
+                            }
+                        ]
+                    }
+                }
+            });
     }
 
     loadInitialData(id) {
@@ -112,9 +177,107 @@ export class ApplicationsItemDetailComponent {
                     this.getApplicationData(id);
                 }
             });
+
+        this.eventSourceAlerts = new EventSource(`${this.eventSourcePath}/alerts`);
+        this.eventSourceMeasures = new EventSource(`${this.eventSourcePath}/measures`);
+
+        
+        this.eventSourceAlerts.onopen = (e) => { console.log(`Connected to ${ e.target.url}`) };
+        this.eventSourceMeasures.onopen = (e) => { console.log(`Connected to ${ e.target.url}`) };
+        
+        this.eventSourceAlerts.onmessage = (e) => { 
+            if (e.data.length) {
+                let response = JSON.parse(e.data);
+                this.alerts.push(response.description);
+                this.changeDetector.detectChanges();
+            };
+        }
+        let classToData = {};
+        this.eventSourceMeasures.onmessage = (e) => { 
+            if (e.data.length) {
+                let serverResponse = JSON.parse(e.data);
+                let labels: string[] = [];
+                console.log(serverResponse);
+
+                let measureClasses = serverResponse.measures.map(m => m.class);
+                measureClasses.sort();
+                let newData = measureClasses
+                    .map(l => serverResponse.measures.filter(m => m.class === l)[0])
+                    .map(entry => entry.cumulativeSum * 100 / entry.total);
+
+                measureClasses.forEach((label, i) => {
+                    if (-1 === Object.keys(classToData).indexOf(label)) {
+                        classToData[label] = {
+                            lineData: [],
+                            barData: []
+                        }
+                    } else {
+                        if (classToData[label].lineData.length > 8) { classToData[label].lineData.shift(); }
+                        if (classToData[label].barData.length > 8) { classToData[label].barData.shift(); }
+
+                        let measureValue = newData[i];
+                        let confidenceValue = Object.keys(serverResponse.confidences).indexOf(label) !== -1 ?
+                            serverResponse.confidences[label] * 100 : 0;
+
+                        i === 0 ? labels.push('n') : labels.push(`n-${i}`);
+                        classToData[label].lineData.push(measureValue);
+                        classToData[label].barData.push(confidenceValue);
+
+                    }
+                });
+
+                let measuresDatasets = Object.keys(classToData).map((label, i) => {
+                    return {
+                        type: 'line',
+                        label: `${label} average`,
+                        backgroundColor: this.dummyColorPicker(i),
+                        borderColor: this.dummyColorPicker(i, 0.9),
+                        fill: false,
+                        data: classToData[label].lineData,
+                        yAxisID: 'right-y-axis'
+                    };
+                });
+
+                let confidenceDatasets = Object.keys(classToData).map((label, i) => {
+                    return {
+                        type: 'bar',
+                        label: label,
+                        backgroundColor: this.dummyColorPicker(i, 0.5),
+                        data: classToData[label].barData,
+                        yAxisID: 'left-y-axis'
+                    };
+                });
+
+                // console.log(measuresDatasets)
+                // console.log(confidenceDatasets)
+
+                let datasets = [...measuresDatasets, ...confidenceDatasets];
+
+                console.log(datasets);
+                console.log('_____________');
+
+                this.chart.data.labels = labels.reverse();
+                this.chart.data.datasets = datasets;
+                this.chart.update({
+                    duration: 0
+                });
+            };
+        }
+    }
+
+    dummyColorPicker(i = 0, opacity = 1) {
+        let base = {
+            r: 255 - i*20,
+            g: 0 + i*20,
+            b: 50 + i*10
+        };
+
+        return `rgba(${base.r}, ${base.g}, ${base.b}, ${opacity})`
     }
 
     ngOnDestroy() {
+        this.eventSourceAlerts.close();
+        this.eventSourceMeasures.close();
         if (this.activeRouteSub) {
             this.activeRouteSub.unsubscribe();
         }
