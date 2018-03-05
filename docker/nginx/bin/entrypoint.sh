ee20449f074b8ce3bdc9629e35873e9b6e2e0a24
Zajs <aga.zajs@gmail.com>
2018-03-05 09:59:14 -0800

    Added redirect to Sonar API to nginx.conf

--- docker/nginx/bin/entrypoint.sh
+++ docker/nginx/bin/entrypoint.sh
@@ -3,6 +3,8 @@
 set -eu
 
 sed -i "s/MANAGER_HOST/$MANAGER_HOST/g" /etc/nginx/sites-templates/manager.conf.tmpl
+sed -i "s/SONAR_HOST/$SONAR_HOST/g" /etc/nginx/sites-templates/manager.conf.tmpl
+
 rm /etc/nginx/conf.d/default.conf
 cp /etc/nginx/sites-templates/manager.conf.tmpl /etc/nginx/conf.d/manager.conf
 exec $@
