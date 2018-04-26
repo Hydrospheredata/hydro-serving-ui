0fac6312eba876b371cd9e3a7805c0efe9bf611a
Maxim Tereshin <tereshin93@gmail.com>
2018-04-27 01:36:43 +0300

    Added settings in influx service

--- src/modules/shared/services/influxdb.service.ts
+++ src/modules/shared/services/influxdb.service.ts
@@ -1,76 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Client } from 'elasticsearch';
+import { InfluxDB } from 'influx';
 import { environment } from '@environments/environment';
 
 @Injectable()
 export class InfluxDBService {
 
-    private client: Client;
-    private elasticUrl: string;
+    private client: InfluxDB;
+    private host: string;
+    private port: string;
 
     constructor() {
         if (!this.client) {
             this.connect();
         }
-        this.elasticUrl = environment.production ?
-            `https://${window.location.hostname}:${window.location.port}` :
-            `${environment.host}:${environment.port}`;
+        this.host = environment.production ? `${window.location.hostname}` : `${environment.host}`;
+        this.port = environment.production ? `${window.location.port}` : `${environment.port}`;
     }
 
     connect() {
-        this.client = new Client({
-            host: `${this.elasticUrl}`,
-            log: 'trace',
-            defer: this.defer,
+        this.client = new InfluxDB({
+            host: this.host,
+            port: Number(this.port)
         });
     }
 
     search(query: string) {
-        return this.client.search({
-            index: 'metrics',
-            body: {
-                'size': 10000,
-                'query': {
-                    'bool': {
-                        'must': [
-                            {
-                                'match': {
-                                    'envoy_cluster_name': query
-                                }
-                            },
-                            {
-                                'match': {
-                                    'name': 'envoy_cluster_external_upstream_rq_xx'
-                                }
-                            }
-                        ],
-                        'filter': [
-                            {
-                                'range': {
-                                    '@timestamp': {
-                                        'gt': 'now-1h'
-                                    }
-                                }
-                            }
-                        ]
-                    },
-                },
-                'sort': [
-                    {
-                        '@timestamp': 'asc'
-                    }
-                ]
-            }
-        });
-    }
-
-    private defer() {
-        let resolve, reject, promise;
-        promise = new Promise((_resolve, _reject) => {
-            resolve = _resolve;
-            reject = _reject;
-        });
-        return { resolve, reject, promise };
+        return this.client.query(query);
     }
 }
 
