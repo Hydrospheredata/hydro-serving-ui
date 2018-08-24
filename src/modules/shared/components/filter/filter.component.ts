de93bc6d92409b03cf96beca3e6b555962a55727
Paul Mck <vixtir90@gmail.com>
2018-08-24 11:41:45 +0300

    reactive input, debounce

--- src/modules/shared/components/filter/filter.component.ts
+++ src/modules/shared/components/filter/filter.component.ts
@@ -1,23 +1,51 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core'
 import { Model, Application } from '@shared/models/_index';
+import { FormControl } from '@angular/forms'
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
     selector: 'hydro-filter',
     templateUrl: './filter.component.html'
 }) 
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+    filterControl = new FormControl('');
+    filterControlSub: Subscription;
+
     @Input() data: Array<Model | Application>;
+    @Input() filterType: string = 'byStrValue'
     @Input() filterProp: string = 'name';
+    @Input() debounce: number = 300;
 
-    @Output() filter = new EventEmitter<Array<Application | Model>>();
+    @Output() filterEvent = new EventEmitter<Array<Application | Model>>();
 
-    set searchStr(searchStr: string){
-        const filtredItems = this.filterData(this.data, searchStr);
-        this.filter.emit(filtredItems);
+    ngOnInit(){
+        const observable: Observable<any> = this.filterControl.valueChanges.debounceTime(this.debounce);
+        const observer: Observer<any> = this. getObserverByType(this.filterType);
+        this.filterControlSub = observable.subscribe(observer);
     }
 
-    private filterData(items, searchStr): Array<Application | Model>{
+    private getObserverByType(type: string): Observer<any>{
+        switch (type) {
+            case 'byStrValue':
+                return {
+                    next: (filterStr) => { 
+                        const filteredData = this.filterByStrValue(filterStr);
+                        this.filterEvent.emit(filteredData);
+                    },
+                    complete: () => {},
+                    error: (e) => console.error(e)
+                }
+            default:
+                return {
+                    next: () => this.filterEvent.emit(this.data),
+                    complete: () => {},
+                    error: (e) => console.error(e)
+                }
+        }
+    }
+
+    private filterByStrValue(searchStr: string): Array<Application | Model>{
         const reg = new RegExp(searchStr, 'i');
-        return items.filter(item => reg.test(item[this.filterProp]));
+        return this.data.filter(item => reg.test(item[this.filterProp]));
     }
 }
