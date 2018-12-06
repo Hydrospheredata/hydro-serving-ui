4aa17f154503d8c1a84bba1144d40f1ed4e1df3b
Paul Mck <vixtir90@gmail.com>
2018-12-06 15:57:50 +0300

    jenkins

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,7 @@ def repository = 'hydro-serving-ui'
 def buildFunction = {
   def curVersion = getVersion()
   sh "npm install"
-  sh "npm build-prod"
+  sh "npm run build-prod"
   sh "cp -r ${repository} docker"
   sh "cd docker && docker build -t hydrosphere/serving-manager-ui:${curVersion} ."
 }
