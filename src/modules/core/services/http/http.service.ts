c68427714c065287ccb8546ce8407da4ad376af5
Paul Mck <vixtir90@gmail.com>
2019-06-09 13:02:22 +0300

    omit undefined params

--- src/modules/core/services/http/http.service.ts
+++ src/modules/core/services/http/http.service.ts
@@ -1,99 +1,115 @@
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { debug } from 'util';
 
-type HydroHttpParams = string | { [param: string]: string | string[]; } | HttpParams;
+type HydroHttpParams =
+  | string
+  | { [param: string]: string | string[] }
+  | HttpParams;
 
 interface IHydroHttpOptions {
-    headers?: any;
-    params?: HydroHttpParams;
-    [propName: string]: any;
+  headers?: any;
+  params?: HydroHttpParams;
+  [propName: string]: any;
 }
 
 @Injectable()
 export class HttpService {
-    private baseUrl: string = '';
+  private baseUrl: string = '';
 
-    constructor(
-        public http: HttpClient
-    ) {
-        if (environment.production) {
-            const { protocol, hostname, port } = window.location;
+  constructor(public http: HttpClient) {
+    if (environment.production) {
+      const { protocol, hostname, port } = window.location;
 
-            this.baseUrl = `${protocol}//${hostname}:${port}`;
-        } else {
-            this.baseUrl = `${environment.host}${environment.port ? ':' + environment.port : ''}`;
+      this.baseUrl = `${protocol}//${hostname}:${port}`;
+    } else {
+      this.baseUrl = `${environment.host}${
+        environment.port ? ':' + environment.port : ''
+      }`;
+    }
+  }
+
+  get(url: string, options?: IHydroHttpOptions): Observable<any> {
+    return this.http
+      .get(this.getFullUrl(url), this.hydroOptions(options))
+      .pipe(catchError(err => this.handleError(err)));
+  }
+
+  delete(url: string, options?: IHydroHttpOptions) {
+    return this.http
+      .delete(this.getFullUrl(url), this.hydroOptions(options))
+      .pipe(catchError(err => this.handleError(err)));
+  }
+
+  post(url: string, body, options?: IHydroHttpOptions) {
+    return this.http
+      .post(this.getFullUrl(url), body, this.hydroOptions(options))
+      .pipe(catchError(err => this.handleError(err)));
+  }
+
+  put(url: string, body, options?: IHydroHttpOptions) {
+    return this.http
+      .put(this.getFullUrl(url), body, this.hydroOptions(options))
+      .pipe(catchError(err => this.handleError(err)));
+  }
+
+  private getFullUrl(url: string): string {
+    return `${this.baseUrl}${url}`;
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<string> {
+    let message: string;
+    if (error.error instanceof ErrorEvent) {
+      message = `An error occurred: ${error.error.message}`;
+    } else {
+      const status: number = error.status;
+
+      let err: string = '';
+      let information: string = '';
+      if (error.error) {
+        err = error.error.error || error.name || '';
+        information = error.error.information || error.message || '';
+      }
+
+      message = `status: ${status}, error: ${err}, message: ${information} `;
+    }
+    return throwError(message);
+  }
+
+  private hydroOptions(options: IHydroHttpOptions = {}) {
+    if (options === null) {
+      return {};
+    }
+
+    return {
+      ...options,
+      params: this.createHttpParams(options.params),
+    };
+  }
+
+  private createHttpParams(
+    params: string | { [param: string]: string | string[] } | HttpParams = ''
+  ): HttpParams {
+    if (params instanceof HttpParams) {
+      return params;
+    } else if (typeof params === 'string') {
+      return new HttpParams({ fromString: params });
+    } else if (typeof params === 'object') {
+      for (const key in params) {
+        if (params.hasOwnProperty(key)) {
+          if (params[key] === undefined) {
+            delete params[key];
+          }
         }
+      }
+      return new HttpParams({ fromObject: params });
     }
-
-    get(url: string, options?: IHydroHttpOptions): Observable<any> {
-        return this.http.get(this.getFullUrl(url), this.hydroOptions(options)).pipe(
-            catchError(err => this.handleError(err))
-        );
-    }
-
-    delete(url: string, options?: IHydroHttpOptions) {
-        return this.http.delete(this.getFullUrl(url), this.hydroOptions(options)).pipe(
-            catchError(err => this.handleError(err))
-        );
-    }
-
-    post(url: string, body, options?: IHydroHttpOptions) {
-        return this.http.post(this.getFullUrl(url), body, this.hydroOptions(options)).pipe(
-            catchError(err => this.handleError(err))
-        );
-    }
-
-    put(url: string, body, options?: IHydroHttpOptions) {
-        return this.http.put(this.getFullUrl(url), body, this.hydroOptions(options)).pipe(
-            catchError(err => this.handleError(err))
-        );
-    }
-
-    private getFullUrl(url: string): string {
-        return `${this.baseUrl}${url}`;
-    }
-
-    private handleError(error: HttpErrorResponse): Observable<string> {
-        let message: string;
-        if (error.error instanceof ErrorEvent) {
-            message = `An error occurred: ${error.error.message}`;
-        } else {
-            const status: number = error.status;
-
-            let err: string = '';
-            let information: string = '';
-            if (error.error) {
-                err = error.error.error || error.name || '';
-                information = error.error.information || error.message || '';
-            }
-
-            message = `status: ${status}, error: ${err}, message: ${information} `;
-        }
-        return throwError(message);
-    }
-
-    private hydroOptions(options: IHydroHttpOptions = {}) {
-        if (options === null) {
-            return {};
-        }
-
-        return {
-            ...options,
-            params: this.createHttpParams(options.params),
-        };
-    }
-
-    private createHttpParams(params: string | { [param: string]: string | string[]; } | HttpParams = ''): HttpParams {
-        if (params instanceof HttpParams) {
-            return params;
-        } else if (typeof params === 'string') {
-            return new HttpParams({ fromString: params });
-        } else if (typeof params === 'object') {
-            return new HttpParams({ fromObject: params });
-        }
-    }
+  }
 }
