5081e99a27fa917fc1976e631d475e26e4735f96
Paul Mck <vixtir90@gmail.com>
2019-06-26 14:06:13 +0300

    tests

--- src/modules/models/components/model-version-log/model-version-log.component.spec.ts
+++ src/modules/models/components/model-version-log/model-version-log.component.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModelVersionLogService } from '@models/services/model-version-log.service';
+import { SharedModule } from '@shared/shared.module';
 import { MockModelVersion1Model1 } from '@testing/factories/modelVersion';
+import { of } from 'rxjs';
 import { ModelVersionLogComponent } from './model-version-log.component';
 
 const MockService = {
-  getLog() {},
+  getLog() { return of([]); },
 };
 
 describe('ModelVersionLogComponent', () => {
@@ -15,6 +17,7 @@ describe('ModelVersionLogComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ ModelVersionLogComponent ],
+      imports: [SharedModule],
       providers: [{
         provide: ModelVersionLogService, useValue: MockService,
       }],
