0fe2f8825523ad3ab6cf699ecac44286081bc7be
Paul Mck <vixtir90@gmail.com>
2019-07-01 16:42:30 +0300

    mock store

--- src/modules/monitoring/components/chart/chart.component.spec.ts
+++ src/modules/monitoring/components/chart/chart.component.spec.ts
@@ -3,9 +3,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MdlSelectModule } from '@angular-mdl/select';
 import { MetricSettingsService } from '@core/services/metrics/_index';
 import { MonitoringService } from '@core/services/metrics/monitoring.service';
-import { provideMockStore } from '@ngrx/store/testing';
+import * as fromModels from '@models/reducers';
+import { Store, MemoizedSelector } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { ModelVersion } from '@shared/_index';
 import { SharedModule } from '@shared/shared.module';
 import { MockMetricSpecification } from '@testing/factories/metric-specification';
+import { MockModelVersion1Model1 } from '@testing/factories/modelVersion';
 import { HsD3Module } from '../../../hs-d3/hs-d3.module';
 import { ChartComponent } from './chart.component';
 
@@ -18,7 +22,8 @@ const MockMonitoringService = {
 describe('ChartComponent', () => {
   let component: ChartComponent;
   let fixture: ComponentFixture<ChartComponent>;
-
+  let store: MockStore<fromModels.State>;
+  let modelVers: MemoizedSelector<fromModels.State, ModelVersion[]>;
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ChartComponent],
@@ -29,6 +34,12 @@ describe('ChartComponent', () => {
         { provide: MetricSettingsService, useValue: MockMetricSettingService },
       ],
     }).compileComponents();
+
+    store = TestBed.get(Store);
+    modelVers = store.overrideSelector(
+      fromModels.getSiblingVersions,
+      [MockModelVersion1Model1]
+    );
   });
 
   beforeEach(() => {
