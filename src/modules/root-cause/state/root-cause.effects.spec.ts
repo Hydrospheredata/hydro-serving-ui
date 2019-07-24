b0cb15a03aace5ab18552c127e3c321e0c167ad4
Paul Mck <vixtir90@gmail.com>
2019-07-24 17:22:31 +0300

    added fail scenario

--- src/modules/root-cause/state/root-cause.effects.spec.ts
+++ src/modules/root-cause/state/root-cause.effects.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { RootCauseEffects } from '@rootcause/state/root-cause.effects';
-import { cold, hot } from 'jasmine-marbles';
-import { of, Observable } from 'rxjs';
+import { cold } from 'jasmine-marbles';
+import { of, Observable, throwError } from 'rxjs';
 import { RootCauseService } from '../services/root-cause.service';
 import * as actions from './root-cause.actions';
 
@@ -13,6 +13,7 @@ const mockRootCauseService = {
 fdescribe('RootCause effects', () => {
   let effects: RootCauseEffects;
   let actions$: Observable<any>;
+  let service: RootCauseService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,10 +25,12 @@ fdescribe('RootCause effects', () => {
     });
 
     effects = TestBed.get(RootCauseEffects);
+    service = TestBed.get(RootCauseService);
   });
 
   describe('getExplanation effect', () => {
     it('with SUCCESS return right action', () => {
+      const s = spyOn(service, 'getExplanation').and.callThrough();
       const action = actions.GetExplanation({
         requestBody: {
           model: { name: '1', version: '1' },
@@ -43,6 +46,29 @@ fdescribe('RootCause effects', () => {
       const expected$ = cold('---b', { b: completed });
 
       expect(effects.getExplanation$).toBeObservable(expected$);
+      expect(s).toHaveBeenCalled();
+    });
+
+    it('with FAIL return right action', () => {
+      const s = spyOn(service, 'getExplanation').and.callFake(() =>
+        throwError('error')
+      );
+      const action = actions.GetExplanation({
+        requestBody: {
+          model: { name: '1', version: '1' },
+          explained_instance: '',
+        },
+      });
+
+      const completed = actions.GetExplanationFailed({
+        error: 'error',
+      });
+
+      actions$ = cold('---a', { a: action });
+      const expected$ = cold('---b', { b: completed });
+
+      expect(effects.getExplanation$).toBeObservable(expected$);
+      expect(s).toHaveBeenCalled();
     });
   });
 });
