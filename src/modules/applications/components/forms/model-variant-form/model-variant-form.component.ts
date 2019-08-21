1ab0db176c8209305f8ed7d597e15882a02f6879
Paul Mck <vixtir90@gmail.com>
2019-08-21 09:41:47 +0300

    Filtered non released model version in application form

--- src/modules/applications/components/forms/model-variant-form/model-variant-form.component.ts
+++ src/modules/applications/components/forms/model-variant-form/model-variant-form.component.ts
@@ -1,98 +1,96 @@
 import {
-    Component,
-    Input,
-    Output,
-    EventEmitter,
-    OnInit,
-    Self,
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  Self,
 } from '@angular/core';
-import {
-    FormGroup,
-    FormControl
-} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { HydroServingState } from '@core/reducers';
 import { getAllModels } from '@models/reducers';
 import { Store } from '@ngrx/store';
 
 import { ModelVariantFormService } from '@applications/services';
-import { Model, ModelVersion, ISignature } from '@shared/_index';
+import { Model, ModelVersion } from '@shared/_index';
 
-import { Observable, Subscription } from 'rxjs';
-import { take, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'hs-model-variant-form',
-    templateUrl: './model-variant-form.component.html',
-    styleUrls: ['./model-variant-form.component.scss'],
-    providers: [ModelVariantFormService],
+  selector: 'hs-model-variant-form',
+  templateUrl: './model-variant-form.component.html',
+  styleUrls: ['./model-variant-form.component.scss'],
+  providers: [ModelVariantFormService],
 })
 export class ModelVariantFormComponent implements OnInit {
-    @Input() group: FormGroup;
-    @Input() index: number;
-    @Input() showRemoveIcon: boolean = false;
+  @Input() group: FormGroup;
+  @Input() index: number;
+  @Input() showRemoveIcon: boolean = false;
 
-    @Output() delete = new EventEmitter();
+  @Output() delete = new EventEmitter();
 
-    public data;
-    public models$: Observable<Model[]> = this.store.select(getAllModels);
-    public modelVersions$: Observable<any>;
-    public selectedModelVersion$: Observable<ModelVersion>;
+  data;
+  models$: Observable<Model[]> = this.store.select(getAllModels);
+  modelVersions$: Observable<any>;
+  selectedModelVersion$: Observable<ModelVersion>;
 
-    get modelControl(): FormControl {
-        return this.group.get('modelId') as FormControl;
-    }
+  get modelControl(): FormControl {
+    return this.group.get('modelId') as FormControl;
+  }
 
-    get modelVersionControl(): FormControl {
-        return this.group.get('modelVersionId') as FormControl;
-    }
+  get modelVersionControl(): FormControl {
+    return this.group.get('modelVersionId') as FormControl;
+  }
 
-    get weightControl(): FormControl {
-        return this.group.get('weight') as FormControl;
-    }
+  get weightControl(): FormControl {
+    return this.group.get('weight') as FormControl;
+  }
 
-    constructor(
-        private store: Store<HydroServingState>,
-        @Self() private modelVariantFormService: ModelVariantFormService
-    ) {
-        this.modelVersions$ = this.modelVariantFormService.getModelVersions();
-        this.selectedModelVersion$ = this.modelVariantFormService.getCurrentModelVersion();
-    }
+  constructor(
+    private store: Store<HydroServingState>,
+    @Self() private modelVariantFormService: ModelVariantFormService
+  ) {
+    this.modelVersions$ = this.modelVariantFormService.getModelVersions();
+    this.selectedModelVersion$ = this.modelVariantFormService.getCurrentModelVersion();
+  }
 
-    ngOnInit() {
-        this.modelVariantFormService.updateModelVersionList(this.modelControl.value);
+  ngOnInit() {
+    this.modelVariantFormService.updateModelVersionList(
+      this.modelControl.value
+    );
 
-        this.modelVariantFormService.setCurrentModelVersion(this.modelVersionControl.value);
-        this.subscribeToModelIdChange();
-        this.subcribeToModelVersionIdChange();
-    }
+    this.modelVariantFormService.setCurrentModelVersion(
+      this.modelVersionControl.value
+    );
+    this.subscribeToModelIdChange();
+    this.subcribeToModelVersionIdChange();
+  }
 
-    public onModelIdChange(modelId): void {
-        this.modelVariantFormService.updateModelVersionList(modelId);
-        this.modelVersionControl.setValue(this.modelVariantFormService.getDefaultModelVersion().id);
-    }
+  public onModelIdChange(modelId): void {
+    this.modelVariantFormService.updateModelVersionList(modelId);
+    this.modelVersionControl.setValue(
+      this.modelVariantFormService.getDefaultModelVersion().id
+    );
+  }
 
-    public onModelVersionChange(modelVersionId: number): void {
-        this.modelVariantFormService.setCurrentModelVersion(modelVersionId);
-    }
+  public onModelVersionChange(modelVersionId: number): void {
+    this.modelVariantFormService.setCurrentModelVersion(modelVersionId);
+  }
 
-    public onDelete(): void {
-        this.delete.emit(this.index);
-    }
+  public onDelete(): void {
+    this.delete.emit(this.index);
+  }
 
-    private subscribeToModelIdChange(): void {
-        this.modelControl
-            .valueChanges
-            .subscribe(modelId => {
-                this.onModelIdChange(modelId);
-            });
-    }
+  private subscribeToModelIdChange(): void {
+    this.modelControl.valueChanges.subscribe(modelId => {
+      this.onModelIdChange(modelId);
+    });
+  }
 
-    private subcribeToModelVersionIdChange(): void {
-        this.modelVersionControl
-            .valueChanges
-            .subscribe(modelVersionId => {
-                this.onModelVersionChange(modelVersionId);
-            });
-    }
+  private subcribeToModelVersionIdChange(): void {
+    this.modelVersionControl.valueChanges.subscribe(modelVersionId => {
+      this.onModelVersionChange(modelVersionId);
+    });
+  }
 }
--- src/modules/applications/services/model-variant-form.service.ts
+++ src/modules/applications/services/model-variant-form.service.ts
@@ -4,124 +4,143 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
 import { HydroServingState } from '@core/reducers';
-import { getAllModelVersions, getModelVersionsByModelId, getAllModels } from '@models/reducers';
+import {
+  getAllModelVersions,
+  getModelVersionsByModelId,
+  getAllModels,
+} from '@models/reducers';
 
 import { CustomValidatorsService } from '@core/services/custom-validators.service';
-import { ModelVersion, Model, IModelVariant } from '@shared/_index';
+import {
+  ModelVersion,
+  Model,
+  IModelVariant,
+  ModelVersionStatus,
+} from '@shared/_index';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import {take, tap} from 'rxjs/operators';
+import { take, tap, map } from 'rxjs/operators';
 
 export interface IModelVariantFormData {
-    weight: number;
-    modelId?: number;
-    modelVersionId: number;
+  weight: number;
+  modelId?: number;
+  modelVersionId: number;
 }
 
 @Injectable()
 export class ModelVariantFormService implements OnDestroy {
-    private allModelVersions: ModelVersion[];
-    private allModelVersionsSub: Subscription;
-    private modelVersions = new BehaviorSubject<any[]>([]);
-    private models: Model[];
-    private modelsSub: Subscription;
+  private allModelVersions: ModelVersion[];
+  private allModelVersionsSub: Subscription;
+  private modelVersions = new BehaviorSubject<any[]>([]);
+  private models: Model[];
+  private modelsSub: Subscription;
 
-    private currentModelVersion = new BehaviorSubject<any>({});
+  private currentModelVersion = new BehaviorSubject<any>({});
 
-    constructor(
-        private store: Store<HydroServingState>,
-        private customValidators: CustomValidatorsService
-    ) {
-        this.allModelVersionsSub = this.store.select(getAllModelVersions).subscribe(
-            allModelVersions => this.allModelVersions = allModelVersions
-        );
+  constructor(
+    private store: Store<HydroServingState>,
+    private customValidators: CustomValidatorsService
+  ) {
+    this.allModelVersionsSub = this.store
+      .select(getAllModelVersions)
+      .subscribe(
+        allModelVersions => (this.allModelVersions = allModelVersions)
+      );
 
-        this.modelsSub = store.select(getAllModels).subscribe(
-            models => this.models = models
-        );
+    this.modelsSub = store
+      .select(getAllModels)
+      .subscribe(models => (this.models = models));
+  }
+
+  public getModelVersions(): Observable<any> {
+    return this.modelVersions.asObservable();
+  }
+
+  public updateModelVersionList(modelId: number): void {
+    this.store
+      .select(getModelVersionsByModelId(modelId))
+      .pipe(
+        take(1),
+        map(modelVersions =>
+          modelVersions.filter(mv => mv.status === ModelVersionStatus.Released)
+        ),
+        tap(modelVersions => {
+          this.modelVersions.next(modelVersions);
+        })
+      )
+      .subscribe();
+  }
+
+  public getDefaultModelVersion(): ModelVersion {
+    const currentModelVersionsArray: ModelVersion[] = this.modelVersions.getValue();
+
+    if (currentModelVersionsArray.length) {
+      return currentModelVersionsArray[0];
     }
+  }
 
-    public getModelVersions(): Observable<any> {
-        return this.modelVersions.asObservable();
+  public defaultModelVariantFormData(): IModelVariantFormData {
+    const modelId = this.defaultModelId();
+    this.updateModelVersionList(modelId);
+    const modelVersion = this.getDefaultModelVersion();
+
+    return {
+      weight: 100,
+      modelId,
+      modelVersionId: modelVersion && modelVersion.id,
+    };
+  }
+
+  public modelVariantToModelVariantFormData(
+    modelVariant: IModelVariant
+  ): IModelVariantFormData {
+    return {
+      weight: modelVariant.weight,
+      modelId: modelVariant.modelVersion.model.id,
+      modelVersionId: modelVariant.modelVersion.id,
+    };
+  }
+
+  public buildModelVariantFormGroup(
+    modelVariantFormData: IModelVariantFormData = this.defaultModelVariantFormData()
+  ): FormGroup {
+    return new FormGroup({
+      weight: new FormControl(modelVariantFormData.weight, [
+        this.customValidators.required(),
+        this.customValidators.pattern(/^[0-9]+$/),
+      ]),
+      modelId: new FormControl(
+        modelVariantFormData.modelId,
+        this.customValidators.required()
+      ),
+      modelVersionId: new FormControl(
+        modelVariantFormData.modelVersionId,
+        this.customValidators.required()
+      ),
+    });
+  }
+
+  public getCurrentModelVersion(): Observable<ModelVersion> {
+    return this.currentModelVersion.asObservable();
+  }
+
+  public setCurrentModelVersion(modelVersionId): void {
+    const modelVersion = this.allModelVersions.find(
+      mV => mV.id === modelVersionId
+    );
+
+    if (modelVersion) {
+      this.currentModelVersion.next(modelVersion);
     }
+  }
 
-    public updateModelVersionList(modelId: number): void {
-        this.store
-            .select(getModelVersionsByModelId(modelId))
-            .pipe(
-                take(1),
-                tap(modelVersions => {
-                    this.modelVersions.next(modelVersions);
-                })
-            ).subscribe();
+  ngOnDestroy() {
+    this.modelsSub.unsubscribe();
+    this.allModelVersionsSub.unsubscribe();
+  }
+
+  private defaultModelId() {
+    if (this.models[0]) {
+      return this.models[0].id;
     }
-
-    public getDefaultModelVersion(): ModelVersion {
-        const currentModelVersionsArray: ModelVersion[] = this.modelVersions.getValue();
-
-        if (currentModelVersionsArray.length) {
-            return currentModelVersionsArray[0];
-        }
-    }
-
-    public defaultModelVariantFormData(): IModelVariantFormData {
-        const modelId = this.defaultModelId();
-        this.updateModelVersionList(modelId);
-        const modelVersion = this.getDefaultModelVersion();
-
-        return {
-            weight: 100,
-            modelId,
-            modelVersionId: modelVersion && modelVersion.id,
-        };
-    }
-
-    public modelVariantToModelVariantFormData(modelVariant: IModelVariant): IModelVariantFormData {
-        return {
-            weight: modelVariant.weight,
-            modelId: modelVariant.modelVersion.model.id,
-            modelVersionId: modelVariant.modelVersion.id,
-        };
-    }
-
-    public buildModelVariantFormGroup(
-        modelVariantFormData: IModelVariantFormData = this.defaultModelVariantFormData()
-    ): FormGroup {
-        return new FormGroup({
-            weight: new FormControl(
-                modelVariantFormData.weight,
-                [this.customValidators.required(), this.customValidators.pattern(/^[0-9]+$/)]
-            ),
-            modelId: new FormControl(
-                modelVariantFormData.modelId,
-                this.customValidators.required()
-            ),
-            modelVersionId: new FormControl(
-                modelVariantFormData.modelVersionId,
-                this.customValidators.required()
-            ),
-        });
-    }
-
-    public getCurrentModelVersion(): Observable<ModelVersion> {
-        return this.currentModelVersion.asObservable();
-    }
-
-    public setCurrentModelVersion(modelVersionId): void {
-        const modelVersion = this.allModelVersions.find(mV => mV.id === modelVersionId);
-
-        if (modelVersion) {
-            this.currentModelVersion.next(modelVersion);
-        }
-    }
-
-    ngOnDestroy() {
-        this.modelsSub.unsubscribe();
-        this.allModelVersionsSub.unsubscribe();
-    }
-
-    private defaultModelId() {
-        if (this.models[0]) {
-            return this.models[0].id;
-        }
-    }
+  }
 }
