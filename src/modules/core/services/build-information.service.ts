c5cd2127aa1f966db59a211f4a5b1b32262d2865
Paul Mck <vixtir90@gmail.com>
2019-12-10 14:37:48 +0300

    added sonar

--- src/modules/core/services/build-information.service.ts
+++ src/modules/core/services/build-information.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '@core/services/http';
-import { Observable } from 'rxjs';
+import { Observable, of, forkJoin } from 'rxjs';
 import { combineLatest } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class BuildInformationService {
   private buildInfoApi = '/api/buildinfo';
   private gatewayInfoApi = '/gateway/buildinfo';
+  private sonarApi = '/monitoring/buildinfo';
+  private rootcauseApi = '/rootcause/buildInfo';
 
   constructor(private http: HttpService) {}
 
   getBuildInformation(): Observable<any> {
-    return combineLatest(
-      this.http.get(this.buildInfoApi),
-      this.http.get(this.gatewayInfoApi)
-    );
+    const toRequest = endpoint =>
+      this.http.get(endpoint).pipe(catchError(_ => of([])));
+
+    return forkJoin({
+      build: toRequest(this.buildInfoApi),
+      gateway: toRequest(this.gatewayInfoApi),
+      sonar: toRequest(this.sonarApi),
+      // rootcause: toRequest(this.rootcauseApi),
+    });
   }
 }
