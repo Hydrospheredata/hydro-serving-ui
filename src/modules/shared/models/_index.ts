810075d126bf025e32cc04a5bfe35e3063d5d765
Paul Mck <vixtir90@gmail.com>
2020-03-18 11:24:19 +0300

    removed unused interfaces

--- src/modules/shared/models/_index.ts
+++ src/modules/shared/models/_index.ts
@@ -1,12 +1,8 @@
 export * from './application.model';
-export * from './model.model';
 export * from './environment.model';
-
-export * from './model-version.model';
-export * from './signature.model';
-export * from './source.model';
-
-export * from './profiles.model';
-export * from './image.model';
 export * from './hostSelector.model';
-export * from './sonar-response.model';
+export * from './image.model';
+export * from './model-version.model';
+export * from './model.model';
+export * from './profiles.model';
+export * from './signature.model';
