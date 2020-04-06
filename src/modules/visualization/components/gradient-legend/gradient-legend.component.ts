e1dae12c483fe1273749d5e736b4f206658eb9e3
Paul Mck <vixtir90@gmail.com>
2020-04-06 11:47:29 +0300

    refacrored gradient legend

--- src/modules/visualization/components/gradient-legend/gradient-legend.component.ts
+++ src/modules/visualization/components/gradient-legend/gradient-legend.component.ts
@@ -18,36 +18,38 @@ export class GradientLegendComponent implements AfterViewInit {
   @ViewChild('anchor', { read: ElementRef }) anchorElement: ElementRef;
   ngAfterViewInit() {
     const colorScale = d3.scaleSequential(d3.interpolateRdBu).domain([0, 1]);
-    const legendHeight = 200;
-    const legendWidth = 1056;
-    const margin = { top: 10, right: 60, bottom: 10, left: 2 };
+    const legendHeight = 48;
+    const legendWidth = this.anchorElement.nativeElement.offsetWidth;
+    const gradientHeight = 24;
+    const margin = { top: 10, right: 20, bottom: 10, left: 4 };
+
+    const viewWidth = legendWidth - margin.right - margin.left;
 
     const canvas = d3
       .select(this.anchorElement.nativeElement)
-      .style('height', 48 + 'px')
+      .style('height', legendHeight + 'px')
       .style('width', legendWidth + 'px')
       .style('position', 'relative')
       .append('canvas')
-      .attr('class', 'leg')
-      .attr('height', legendHeight - margin.top - margin.bottom)
-      .attr('width', 1)
-      .style('height', '1056px')
-      .style('width', '20px')
+      .attr('class', 'legend')
+      .attr('height', 1)
+      .attr('width', viewWidth)
+      .style('height', gradientHeight + 'px')
+      .style('width', viewWidth + 'px')
       .style('border', '1px solid #000')
       .style('position', 'absolute')
-      .style('transform-origin', '10px 10px')
-      .style('transform', 'rotate(-90px)')
       .node();
 
     const ctx = canvas.getContext('2d');
 
     const legendScale = d3
       .scaleLinear()
-      .range([1, legendHeight - margin.top - margin.bottom])
+      .range([1, viewWidth])
       .domain(colorScale.domain());
+    // 1034
 
-    const image = ctx.createImageData(1, legendHeight);
-    d3.range(legendHeight).forEach(i => {
+    const image = ctx.createImageData(viewWidth, 1);
+    d3.range(viewWidth).forEach(i => {
       const { r, g, b } = d3.rgb(colorScale(legendScale.invert(i)));
       image.data[4 * i] = r;
       image.data[4 * i + 1] = g;
@@ -58,23 +60,21 @@ export class GradientLegendComponent implements AfterViewInit {
 
     const legendAxis = d3
       .axisBottom(legendScale)
-      .scale(d3.scaleLinear().range([0, 1056]).domain([0, 1]))
+      .scale(d3.scaleLinear().range([0, viewWidth - 1]).domain([0, 1]))
       .tickSize(6)
       .ticks(8);
 
     const svg = d3
       .select(this.anchorElement.nativeElement)
       .append('svg')
-      .attr('height', 48 + 'px')
-      .attr('width', legendWidth + 'px')
-      .style('position', 'absolute')
-      .style('left', '0px')
-      .style('top', '0px');
+      .attr('height', legendHeight + 'px')
+      .attr('width', viewWidth + 8 + 'px')
+      .style('position', 'absolute');
 
     svg
       .append('g')
       .attr('class', 'axis')
-      .style('transform', 'translate(-1px, 19px)')
+      .style('transform', `translate(0px, ${gradientHeight - 1}px)`)
       .call(legendAxis);
   }
 }
