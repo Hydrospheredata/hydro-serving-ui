847db9b0282424db07d63282e6179a491bb4df17
Paul Mck <vixtir90@gmail.com>
2020-05-13 22:09:50 +0300

    add store to stat module

--- src/modules/stat/containers/stat-page/stat-page.component.ts
+++ src/modules/stat/containers/stat-page/stat-page.component.ts
@@ -4,31 +4,28 @@ import { catchError } from '@node_modules/rxjs/internal/operators';
 import { ModelVersion } from '@shared/models';
 import { Stat } from '../../models';
 import { StatFacade } from '../../stat.facade';
+import { StatState } from '../../store/stat.state';
 
 @Component({
   templateUrl: './stat-page.component.html',
   styleUrls: ['./stat-page.component.scss'],
-  providers: [StatFacade],
+  providers: [StatState, StatFacade],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatPageComponent implements OnInit {
   stat$: Observable<Stat>;
   error$: Observable<string>;
   modelVersion$: Observable<ModelVersion>;
-  private error: BehaviorSubject<string> = new BehaviorSubject<string>(
-    undefined
-  );
+  isLoading$: Observable<boolean>;
 
   constructor(private statFacade: StatFacade) {}
 
   ngOnInit() {
     this.modelVersion$ = this.statFacade.getModelVersion();
-    this.error$ = this.error.asObservable();
-    this.stat$ = this.statFacade.getStat().pipe(
-      catchError(err => {
-        this.error.next(err);
-        return of(null);
-      })
-    );
+    this.error$ = this.statFacade.getError();
+    this.stat$ = this.statFacade.getStat();
+    this.isLoading$ = this.statFacade.isLoading();
+
+    this.statFacade.loadingStat();
   }
 }
--- src/modules/stat/stat.facade.ts
+++ src/modules/stat/stat.facade.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { neitherNullNorUndefined } from '@shared/utils';
 import { StatApiService } from './services/stat-api.service';
 import { Observable } from '@node_modules/rxjs';
 import { Stat } from './models';
 import { ModelsFacade } from '@models/store';
-import { switchMap } from '@node_modules/rxjs/internal/operators';
-import { ModelVersion, Model } from '@shared/models';
+import { switchMap, tap } from '@node_modules/rxjs/internal/operators';
+import { ModelVersion } from '@shared/models';
+import { StatState } from './store/stat.state';
 
 @Injectable()
 export class StatFacade {
@@ -12,24 +14,48 @@ export class StatFacade {
   private readonly modelVersion$: Observable<ModelVersion>;
 
   constructor(
+    private statState: StatState,
     private statApi: StatApiService,
     private modelsFacade: ModelsFacade
   ) {
     this.modelVersion$ = this.modelsFacade.selectedModelVersion$;
-    this.stat$ = this.modelsFacade.selectedModelVersion$.pipe(
-      switchMap(modelVersion =>
-        this.statApi.getStat({
-          model_version_id: `${modelVersion.id}`,
-        })
-      )
-    );
   }
 
   public getStat(): Observable<Stat> {
-    return this.stat$;
+    return this.statState.getStat().pipe(neitherNullNorUndefined);
+  }
+
+  public isLoading(): Observable<boolean> {
+    return this.statState.isLoading();
+  }
+
+  public getError(): Observable<string | null> {
+    return this.statState.getError();
   }
 
   public getModelVersion(): Observable<ModelVersion> {
     return this.modelVersion$;
+  }
+
+  public loadingStat(): void {
+    this.modelVersion$
+      .pipe(
+        tap(() => this.statState.setLoading(true)),
+        switchMap(modelVersion =>
+          this.statApi.getStat({
+            model_version_id: `${modelVersion.id}`,
+          })
+        )
+      )
+      .subscribe(
+        stat => {
+          this.statState.setStat(stat);
+          this.statState.setLoading(false);
+        },
+        error => {
+          this.statState.setLoading(false);
+          this.statState.setError(error);
+        }
+      );
   }
 }
