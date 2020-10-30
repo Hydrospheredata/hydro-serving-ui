94a6ca4693008be807b228de556c3fbd733cfd3c
Ilnur Garifullin <ilnur.email@gmail.com>
2020-10-30 20:27:28 +0300

    Update CommandCreator for gRPC

--- src/modules/core/services/command-creator/grpc-command-creator.ts
+++ src/modules/core/services/command-creator/grpc-command-creator.ts
@@ -1,87 +1,39 @@
-import { Application, ISignature } from '@shared/_index';
+import { environment } from '@environments/environment';
+import { Application } from '@shared/_index';
 import { CommandCreator } from './command-creator';
 
 export class GrpcCommandCreator extends CommandCreator {
 
-    private valueAttributes: { [propName: string]: string } = {
-            DT_HALF : 'half_val',
-            DT_FLOAT : 'float_val',
-            DT_DOUBLE : 'double_val',
-            DT_INT32 : 'int_val',
-            DT_INT16 : 'int_val',
-            DT_INT8 : 'int_val',
-            DT_UINT8 : 'int_val',
-            DT_UINT32 : 'uint32_val',
-            DT_UINT64 : 'uint64_val',
-            DT_STRING : 'string_val',
-            DT_COMPLEX64 : 'complex_val',
-            DT_INT64 : 'int64_val',
-            DT_BOOL : 'bool_val',
-            DT_COMPLEX128 : 'dcomplex_val',
-            DT_VARIANT : 'variant_val',
-    };
+    get apiUrl(): { protocol: string, hostname: string } {
+        if (environment.production) {
+          const { protocol, hostname } = window.location;
+          return { protocol, hostname };
+        } else {
+          const url = new URL(environment.host);
+          return { 'protocol': url.protocol, 'hostname': url.hostname }
+        }
+    }
 
     getCommand(application: Application): string {
-        const { name: appName, input: appInput, signature } = application;
-
-        const dtype = this.getDtype(signature);
-        const typeVal = this.getTypeVal(dtype);
-        const { inputKey, inputValue} = this.getInputKeyValue(appInput);
+        const { name: appName, input: appInput } = application;
+        const { protocol, hostname } = this.apiUrl;
 
         return `import grpc \n
-            import hydro_serving_grpc as hs \n
-            channel = grpc.insecure_channel("localhost") \n
-            stub = hs.PredictionServiceStub(channel) \n
-            model_spec = hs.ModelSpec(name="${appName}", signature_name="${this.getSignatureName(signature)}")\n
+from hydrosdk import Cluster, Application \n
 
-            tensor_shape = hs.TensorShapeProto(dim=[hs.TensorShapeProto.Dim(size=${this.getDimSize()})])\n
-            tensor = hs.TensorProto(dtype=hs.${dtype}, tensor_shape=tensor_shape, ${typeVal}=${inputValue})\n
-            request = hs.PredictRequest(model_spec=model_spec, inputs={"${inputKey}": tensor}) \n
+credentials = grpc.ssl_channel_credentials() \n
+cluster = Cluster( \n
+    http_address="${protocol}//${hostname}", \n
+    grpc_address="${hostname}", \n
+    ssl=True,\t\t\t\t# turn off, if your Hydrosphere instance doesn't have \n
+    grpc_credentials=credentials\t# a TLS certificate installed \n
+) \n
 
-            result = stub.Predict(request) \n
-            `.trim();
-    }
+application = Application.find(cluster, "${appName}") \n
+application.lock_while_starting() \n
+predictor = application.predictor() \n
 
-    private getDimSize(): string {
-        try {
-            // TODO: check
-            // return `${this.contract.signatures.inputs.shape.dim.size}`;
-            return 'dim_size';
-        } catch {
-            return ` %dim size% `;
-        }
-    }
-
-    private getTypeVal(dtype: string): string {
-        return this.valueAttributes[dtype] || '';
-    }
-
-    private getInputKeyValue(appInput: string): {inputKey: string, inputValue: string} {
-        try {
-            const parsedObj = JSON.parse(appInput);
-            const inputKey = Object.keys(parsedObj)[0];
-            const inputValue = JSON.stringify(parsedObj[inputKey]).replace(/,/g, ', ');
-
-            return { inputKey, inputValue };
-        } catch {
-            return { inputKey: ' %input key% ', inputValue: '%input value% ' };
-        }
-    }
-
-    private getSignatureName(signature: ISignature): string {
-        try {
-            return signature.signatureName;
-        } catch {
-            return ``;
-        }
-    }
-
-    private getDtype(signature: ISignature): string {
-        try {
-            // TODO: check
-            return signature.inputs[0].dtype;
-        } catch {
-            return ``;
-        }
+result = predictor.predict(${appInput}) \n
+`.trim();
     }
 }
