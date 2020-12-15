8216801a1ad43f5e7fa1807d1b7eb1f9e0265652
akastav <switch@bk.ru>
2020-12-15 21:30:52 +0300

    initial new release proccess

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,64 +1,144 @@
-pipeline {
-    agent any
+properties([
+  parameters([
+    choice(choices: ['patch','minor','major'], name: 'patchVersion', description: 'What needs to be bump?'),
+    string(defaultValue:'', description: 'Force set newVersion or leave empty', name: 'newVersion', trim: false),
+    choice(choices: ['local', 'global'], name: 'release', description: 'It\'s local release or global?'),
+   ])
+])
 
-  stages {  
-    stage('SCM') {
-      steps {
-        git 'https://github.com/Hydrospheredata/hydro-serving-ui.git'
+SERVICENAME = 'hydro-serving-ui'
+REGISTRYURL = 'harbor.hydrosphere.io/hydro-serving'
+SERVICEIMAGENAME = 'hydro-serving-ui'
+GITHUBREPO  = "github.com/Hydrospheredata/hydro-serving-ui.git"
+
+def checkoutRepo(String repo){
+  git changelog: false, credentialsId: 'HydroRobot_AccessToken', poll: false, url: repo
+}
+
+def getVersion(){
+    try{
+        //remove only quotes
+        version = sh(script: "git rev-parse HEAD", returnStdout: true ,label: "get version").trim()
+        return version
+    }catch(err){
+        return "$err" 
+    }
+}
+
+def bumpVersion(String currentVersion,String newVersion, String patch, String path){
+    sh script: """cat <<EOF> ${WORKSPACE}/bumpversion.cfg
+[bumpversion]
+current_version = 0.0.0
+commit = False
+tag = False
+parse = (?P<major>\\d+)\\.(?P<minor>\\d+)\\.(?P<patch>\\d+)
+serialize =
+    {major}.{minor}.{patch}
+
+EOF""", label: "Set bumpversion configfile"
+    if (newVersion != null && newVersion != ''){ //TODO: needs verify valid semver
+        sh("echo $newVersion > version") 
+    }else{
+        sh("bumpversion $patch $path --config-file '${WORKSPACE}/bumpversion.cfg' --allow-dirty --verbose --current-version '$currentVersion'")   
+    }
+}
+
+def buildDocker(){
+    //run build command and store build tag 
+    newVersion = getVersion() 
+    sh script: "docker build -t hydrosphere/$SERVICEIMAGENAME:$newVersion .", label: "Run build docker task";
+}
+
+def pushDocker(String registryUrl, String dockerImage){
+    //push docker image to registryUrl
+    withCredentials([usernamePassword(credentialsId: 'hydro_harbor_docker_registry', passwordVariable: 'password', usernameVariable: 'username')]) {
+      sh script: "docker login --username $username --password $password $registryUrl"
+      sh script: "docker tag hydrosphere/$dockerImage $registryUrl/$dockerImage",label: "set tag to docker image"
+      sh script: "docker push $registryUrl/$dockerImage",label: "push docker image to registry"
+    }
+}
+
+def updateDockerCompose(String newVersion){
+  dir('docker-compose'){
+    //Change template TODO: Change composename and imagename to $SERVICENAME
+    sh script: "sed \"s/.*image:.*/    image: harbor.hydrosphere.io\\/hydro-serving\\/hydro-serving-ui:$newVersion/g\" hydro-serving-ui.service.template > hydro-serving-ui.compose", label: "sed hydro-serving-ui version"
+    //Merge compose into 1 file
+    composeMerge = "docker-compose"
+    composeService = sh label: "Get all template", returnStdout: true, script: "ls *.compose"
+    list = composeService.split( "\\r?\\n" )
+    for(l in list){
+        composeMerge = composeMerge + " -f $l"
+    }
+    composeMerge = composeMerge + " config > docker-compose.yaml"
+    sh script: "$composeMerge", label:"Merge compose file"
+    sh script: "cp docker-compose.yaml ../docker-compose.yaml"
+  }
+}
+
+def updateHelmChart(String newVersion){
+  dir('helm'){
+    //Change template
+    sh script: "sed -i \"s/.*full:.*/  full: harbor.hydrosphere.io\\/hydro-serving\\/hydro-serving-ui:$newVersion/g\" ui/values.yaml", label: "sed hydro-serving-ui version"
+
+    //Refresh readme for chart
+    sh script: "frigate gen ui --no-credits > ui/README.md"
+
+    //Lint
+    dir('ui'){
+        sh script: "helm dep up", label: "Dependency update"
+        sh script: "helm lint .", label: "Lint auto-od chart"
+        sh script: "helm template -n serving --namespace hydrosphere . > test.yaml", label: "save template to file"
+        sh script: "polaris audit --audit-path test.yaml -f yaml", label: "lint template by polaris"
+        sh script: "polaris audit --audit-path test.yaml -f score", label: "get polaris score"
+        sh script: "rm -rf test.yaml", label: "remove test.yaml"
+    }
+
+    dir('serving'){
+        sh script: "helm dep up", label: "Dependency update"
+        sh script: "helm lint .", label: "Lint all charts"
+        sh script: "helm template -n serving --namespace hydrosphere . > test.yaml", label: "save template to file"
+        sh script: "polaris audit --audit-path test.yaml -f yaml", label: "lint template by polaris"
+        sh script: "polaris audit --audit-path test.yaml -f score", label: "get polaris score"
+        sh script: "rm -rf test.yaml", label: "remove test.yaml"
+    }
+  }
+}
+
+node('hydrocentral') {
+    stage('SCM'){
+      checkoutRepo("https://github.com/Hydrospheredata/$SERVICENAME" + '.git')
+      withCredentials([usernamePassword(credentialsId: 'HydroRobot_AccessToken', passwordVariable: 'Githubpassword', usernameVariable: 'Githubusername')]) {
+        //Get Hydro-serving version
+        hydroServingVersion = sh(script: "git ls-remote --tags --sort='v:refname' --refs 'https://$Githubusername:$Githubpassword@github.com/Hydrospheredata/hydro-serving.git' | sed \"s/.*\\///\" | grep -v \"[a-z]\" | tail -n1", returnStdout: true, label: "get global hydrosphere version").trim()
+      }
+    }
+       
+    stage('Test'){
+      if (env.CHANGE_ID != null){
+        buildDocker()
       }
     }
 
-    stage('SonarQube analysis') {
-      steps{
-        script {
-          def scannerHome = tool 'Sonarcloud';
-          withSonarQubeEnv('Sonarcloud') { // If you have configured more than one global server connection, you can specify its name
-            if (env.CHANGE_ID == null) {
-              sh "${scannerHome}/bin/sonar-scanner \
-                -Dsonar.projectKey=Hydrospheredata_hydro-serving-ui \
-                -Dsonar.organization=hydrosphere \
-                -Dsonar.sources=. \
-                -Dsonar.branch.name=${env.BRANCH_NAME} \
-                -Dsonar.host.url=https://sonarcloud.io \
-                -Dsonar.login=ae874d8955687b148ddf4190cc04fbf6376fa0f4"
-            } else {
-              sh "${scannerHome}/bin/sonar-scanner \
-                -Dsonar.projectKey=Hydrospheredata_hydro-serving-ui \
-                -Dsonar.organization=hydrosphere \
-                -Dsonar.sources=. \
-                -Dsonar.pullrequest.key=${env.CHANGE_ID} \
-                -Dsonar.pullrequest.branch=${env.CHANGE_BRANCH} \
-                -Dsonar.pullrequest.base=${env.CHANGE_TARGET} \
-                -Dsonar.host.url=https://sonarcloud.io \
-                -Dsonar.login=ae874d8955687b148ddf4190cc04fbf6376fa0f4"
+    stage('Release'){
+      if (BRANCH_NAME == 'master' || BRANCH_NAME == 'main'){
+          // oldVersion = getVersion()
+          // bumpVersion(getVersion(),params.newVersion,params.patchVersion,'version')
+          newVersion = getVersion()
+          buildDocker()
+          pushDocker(REGISTRYURL, SERVICEIMAGENAME+":$newVersion")
+          //Update helm and docker-compose if release 
+        if (params.release == 'local'){
+          dir('release'){
+            //bump only image tag
+            withCredentials([usernamePassword(credentialsId: 'HydroRobot_AccessToken', passwordVariable: 'Githubpassword', usernameVariable: 'Githubusername')]) {
+              git changelog: false, credentialsId: 'HydroRobot_AccessToken', url: "https://$Githubusername:$Githubpassword@github.com/Hydrospheredata/hydro-serving.git"      
+              updateHelmChart("$newVersion")
+              updateDockerCompose("$newVersion")
+              sh script: "git commit --allow-empty -a -m 'Releasing $SERVICENAME:$newVersion'",label: "commit to git chart repo"
+              sh script: "git push https://$Githubusername:$Githubpassword@github.com/Hydrospheredata/hydro-serving.git --set-upstream master",label: "push to git"
             }
           }
         }
       }
     }
-
-//  stage("Quality Gate"){
-//    timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
-//      def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
-//      if (qg.status != 'OK') {
-//        error "Pipeline aborted due to quality gate failure: ${qg.status}"
-//      }
-//    }
-//  }
-
-    stage("trigger-central") {
-      steps {
-        build job: 'provectus.com/hydro-central/master', parameters: [
-          [$class: 'StringParameterValue',
-          name: 'PROJECT',
-          value: 'ui'
-          ],
-          [$class: 'StringParameterValue',
-          name: 'BRANCH',
-          value: env.BRANCH_NAME
-          ]
-        ]
-      }
-    }
-  }
 }
