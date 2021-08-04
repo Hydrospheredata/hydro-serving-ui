1d368e8d566f6f561d3cf78a1a6a19102c4cde85
Paul Mck <vixtir90@gmail.com>
2021-08-04 16:19:54 +0300

    refactor: simplify model uploading example

--- src/app/helpers/model-uploading-commands.ts
+++ src/app/helpers/model-uploading-commands.ts
@@ -1,9 +1,6 @@
 export const commands: string[] = [
   'pip install hs',
   'git clone https://github.com/Hydrospheredata/hydro-serving-example.git',
-  'cd hydro-serving-example/examples/custom_metrics/census/data',
-  'dvc pull train.csv.dvc',
-  'cd hydro-serving-example/examples/custom_metrics/census',
-  'dvc pull',
+  'cd examples/custom_metrics/census/models/model',
   'hs apply -f serving.yaml',
 ];
